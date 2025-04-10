d027834faed1a9fdfe3b89897de2992eb2924979
ketan1321 <ketankawade10@gamail.com>
2025-04-10 14:54:24 +0530

    Second imp Commit

--- BubbleSort/Bubble.java
+++ BubbleSort/Bubble.java
@@ -1,3 +1,19 @@
-public class Bubble {
+import java.util.Arrays;
 
+public class Bubble {
+    public static void main(String[] args) {
+        int[] arr = { 86, 15, 84, 93, 73, 97, 69, 2 };
+        int n = arr.length;
+        for (int i = 0; i < n - 1; i++) {
+            int temp;
+            for (int j = 0; j < n - 1 - i; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    temp = arr[i];
+                    arr[i] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+        System.out.println(Arrays.toString(arr));
+    }
 }
--- SelectionSort/Selection.java
+++ SelectionSort/Selection.java
@@ -1,5 +1,22 @@
 package SelectionSort;
 
-public class Selection {
+import java.util.Arrays;
 
+public class Selection {
+    public static void main(String[] args) {
+        int[] array = { 86, 15, 84, 93, 73, 97, 69, 2 };
+        int n = array.length;
+        for (int i = 0; i < n - 1; i++) {
+            int min_index = i;
+            for (int j = i + 1; j < n; j++) {
+                if (array[j] < array[min_index]) {
+                    min_index = j;
+                }
+            }
+            int temp = array[min_index];
+            array[min_index] = array[i];
+            array[i] = temp;
+        }
+        System.out.println(Arrays.toString(array));
+    }
 }

50a64c4dc20f18cac3179d9be3ef696b2fea5ee0
ketan1321 <ketankawade10@gamail.com>
2025-04-10 15:10:28 +0530

    Changes

--- SelectionSort/Selection.java
+++ SelectionSort/Selection.java
@@ -4,19 +4,21 @@ import java.util.Arrays;
 
 public class Selection {
     public static void main(String[] args) {
-        int[] array = { 86, 15, 84, 93, 73, 97, 69, 2 };
-        int n = array.length;
+        System.out.println("Before Sorting : ");
+        int[] arr = { 86, 15, 84, 93, 73, 97, 69, 2 };
+        int n = arr.length;
         for (int i = 0; i < n - 1; i++) {
             int min_index = i;
             for (int j = i + 1; j < n; j++) {
-                if (array[j] < array[min_index]) {
+                if (arr[j] < arr[min_index]) {
                     min_index = j;
                 }
             }
-            int temp = array[min_index];
-            array[min_index] = array[i];
-            array[i] = temp;
+            int temp = arr[min_index];
+            arr[min_index] = arr[i];
+            arr[i] = temp;
         }
-        System.out.println(Arrays.toString(array));
+        System.out.println("After Sorting :");
+        System.out.println(Arrays.toString(arr));
     }
 }
